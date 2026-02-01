637999aeef2e829516c6d71ed7abf614ad52cbf4
ftugrulaygun <fevzi.tugrul40@gmail.com>
2026-02-01 08:14:26 +0300

    Opened up Main.java file for 'free to test' usage to the user.

--- src/main/java/com/pizzashop/Main.java
+++ src/main/java/com/pizzashop/Main.java
@@ -9,22 +9,6 @@ import com.pizzashop.models.decorators.PepperoniDecorator;
 
 public class Main{
     public static void main(String[] args){
-        Pizza pizza = new BasePizza();
-
-        pizza = new CheeseDecorator(pizza);
-        System.out.println(pizza.getDescription());
-        System.out.println("Cost: $" + pizza.getCost());
-
-        pizza = new PepperoniDecorator(pizza);
-        System.out.println(pizza.getDescription());
-        System.out.println("Cost: $" + pizza.getCost());
-
-        pizza = new OliveDecorator(pizza);
-        System.out.println(pizza.getDescription());
-        System.out.println("Cost: $" + pizza.getCost());
-
-        pizza = new MushroomDecorator(pizza);
-        System.out.println(pizza.getDescription());
-        System.out.println("Cost: $" + pizza.getCost());
+        
     }
 }
