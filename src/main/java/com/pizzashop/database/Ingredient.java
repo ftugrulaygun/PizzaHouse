741ddff1d15a2b54ce5160b9af23844c838f7206
ftugrulaygun <fevzi.tugrul40@gmail.com>
2026-02-04 20:04:21 +0300

    ADD: Added Ingredient class

---
+++ src/main/java/com/pizzashop/database/Ingredient.java
@@ -0,0 +1,42 @@
+package com.pizzashop.database;
+
+public class Ingredient extends DatabaseManager{
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String unit;
+    
+    public Ingredient(int id, String name, double price , int quantity, String unit){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+        public String getName(){
+            return name;
+        }
+       
+        public int getQuantity(){
+            return quantity;
+        }
+
+        public double getPrice(){
+            return price;
+        }
+
+        public String getUnit(){
+            return unit;
+        }
+
+        public int getID(){
+            return id;
+        }
+
+        public void setQuantity(int appliedQuantity){
+            this.quantity = appliedQuantity;
+        }
+}
+
