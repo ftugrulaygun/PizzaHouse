ffe893f94eef26aec6ec3d6aa5fe60c8c6cf1e2b
ftugrulaygun <fevzi.tugrul40@gmail.com>
2026-02-08 14:38:50 +0300

    FIX: Implementation on basic testing.

--- src/main/java/com/pizzashop/Main.java
+++ src/main/java/com/pizzashop/Main.java
@@ -6,13 +6,35 @@ import com.pizzashop.models.decorators.CheeseDecorator;
 import com.pizzashop.models.decorators.MushroomDecorator;
 import com.pizzashop.models.decorators.OliveDecorator;
 import com.pizzashop.models.decorators.PepperoniDecorator;
+import com.pizzashop.database.DatabaseManager;
+import com.pizzashop.database.Ingredient;
 
-public class Main{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args){
         Pizza pizza = new BasePizza();
 
         pizza = new CheeseDecorator(pizza);
         System.out.println(pizza.getDescription());
         System.out.println("Cost: $" + pizza.getCost());
+
+        DatabaseManager db = new DatabaseManager();
+
+        db.initializeTables();
+
+        db.addIngredient("Mozzarella Cheese", 2.50, 1000, "grams");
+        db.addIngredient("Pepperoni", 3.00, 500, "slices");
+        db.addIngredient("Flour", 0.50, 5000, "grams");
+
+        var inventory = db.getAllIngredients();
+
+        for(Ingredient item: inventory){
+            System.out.println(item.getName() + " - " + item.getQuantity());
+        }
+
+        boolean check = db.isIngredientAvailable("Mozzarella Cheese", 700);
+        System.out.println(check);
     }
 }
