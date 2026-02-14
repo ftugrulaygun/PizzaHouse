f08d398b4d25e7c02cfc43a40de053d95cb7d66a
ftugrulaygun <fevzi.tugrul40@gmail.com>
2026-02-14 20:13:24 +0300

    CHANGE: Committing new test cases to Main.java

--- src/main/java/com/pizzashop/Main.java
+++ src/main/java/com/pizzashop/Main.java
@@ -10,33 +10,31 @@ public class Main {
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
 
-        // 1. Setup the Database Tables
         System.out.println("--- Initializing Database ---");
         db.initializeTables();
 
-        // 2. Add Test Ingredients (using your addIngredient method)
-        // Values: Name, Price, Quantity, Unit
         db.addIngredient("Mozzarella", 2.50, 100, "grams");
         db.addIngredient("Pepperoni", 3.00, 50, "slices");
         db.addIngredient("Olives", 1.50, 200, "grams");
 
         System.out.println("\n--- Testing Dynamic Decorators ---");
 
-        // 3. Create a Base Pizza
-        Pizza myPizza = new BasePizza(); // Assume base price is 10.0
+        
+        Pizza myPizza = new BasePizza();
+        Pizza myPizza2 = new BasePizza();
         System.out.println("Base: " + myPizza.getDescription() + " | Cost: $" + myPizza.getCost());
-
-        // 4. Decorate using the Name-based Overload
-        // This will trigger your "SELECT price FROM ingredients WHERE name = ?" logic
+        System.out.println("Base2: " + myPizza2.getDescription() + " | Cost: $" + myPizza2.getCost());
         
 
-// Now you can call the static factory method directly:
-        myPizza = IngredientDecorator.createAndEditPizza(myPizza, "Mozzarella");
-        myPizza = IngredientDecorator.createAndEditPizza(myPizza, "Pepperoni"); // Ingredient obj can be null if methods use name/id
+        
+        myPizza = IngredientDecorator.createAndEditPizza(myPizza, "Mozzarella", 70);
+        myPizza2 = IngredientDecorator.createAndEditPizza(myPizza2, "Mozzarella", 60); 
         
         System.out.println(myPizza.getDescription());
         System.out.println(myPizza.getCost());
+        System.out.println(myPizza2.getDescription());
+        System.out.println(myPizza2.getCost());
 
-        // 5. Decorate using the ID-based Overload (Assuming ID 2 is Pepperoni)
+        
     }
 }
